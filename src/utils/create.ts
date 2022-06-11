214013388601a64de8488ac0ca947134ed92b8d0
bitlatte <github@bitlatte.io>
2022-06-10 20:24:04 -0500

    Added Chalk

--- src/utils/create.ts
+++ src/utils/create.ts
@@ -3,6 +3,7 @@ import { join } from "path";
 import Extractor from "./Extractor";
 import fetch from "./fetch";
 import cleanup from "./cleanup";
+import chalk from "chalk";
 
 const create = (url: string, path: string, type: string) => {
   return new Promise<void>((resolve, reject) => {
@@ -18,7 +19,16 @@ const create = (url: string, path: string, type: string) => {
             (err) => {
               if (err) reject(err);
               cleanup(uuid)
-                .then(resolve)
+                .then(() => {
+                  console.log("\n");
+                  console.log(chalk.yellow(`Project Created: ${path}`));
+                  console.log("\n");
+                  console.log("      To continue:");
+                  console.log("\n");
+                  console.log(`      cd ${path}`);
+                  console.log("      npm install");
+                  console.log("      npm run dev");
+                })
                 .catch((err) => reject(err));
             },
           );
